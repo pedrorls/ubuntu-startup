3985bb9c2c8ad652ea0a961b82883dfa0339bc22
Pedro Rodrigues Lima <perliima72@gmail.com>
2017-01-28 11:59:08 -0200

    password file created for execution as root

---
+++ pass_window.py
@@ -0,0 +1,9 @@
+from PyQt4 import QtGui, QtCore
+import sys
+import os
+
+
+class PassWindow(QtGui.QMainWindow):
+    def __init__(self):
+        super(PassWindow, self).__init__()
+        self.setGeometry(150, 150
