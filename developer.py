927a679b7caf4dbd185061498689ef60e35fe056
Pedro Rodrigues Lima <perliima72@gmail.com>
2017-02-13 19:32:00 -0200

    Some improvements made on developer file

--- developer.py
+++ developer.py
@@ -12,29 +12,24 @@ developer = {
     "JDK":
         "sudo apt-get install default-jdk",
     "Docker":
-        """sudo apt-get update && sudo apt-key
-         adv --keyserver hkp://p80.pool.sks-keyservers.net:80
-          --recv-keys 58118E89F3A912897C070ADBF76221572C52609D &&
-         sudo apt-add-repository
-         'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-         && sudo apt-get update sudo apt-get install -y docker-engine""",
+        "sudo apt-get update && sudo apt-key" +
+        " adv --keyserver hkp://p80.pool.sks-keyservers.net:80" +
+        " --recv-keys 58118E89F3A912897C070ADBF76221572C52609D &&" +
+        " sudo apt-add-repository" +
+        " 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'" +
+        " && sudo apt-get update sudo apt-get install -y docker-engine",
     "Ruby2.4.0":
-        """sudo apt-get update && sudo apt-get install
-        git-core curl zlib1g-dev build-essential libssl-dev
-        libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev
-        libxslt1-dev libcurl4-openssl-dev python-software-properties
-        libffi-dev nodejs
-        cd
-        git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-        echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-        echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-        exec $SHELL
-        git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-        echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-        exec $SHELL
-        rbenv install 2.4.0 && rbenv global 2.4.0 && ruby -v
-        """,
+        "sudo apt-get install libgdbm-dev libncurses5-dev automake libtool" +
+        " bison libffi-dev" +
+        " && gpg --keyserver hkp://keys.gnupg.net --recv-keys" +
+        " 409B6B1796C275462A1703113804BB82D39DC0E3" +
+        " && curl -sSL https://get.rvm.io | bash -s stable" +
+        " && source ~/.rvm/scripts/rvm" +
+        " && rvm install 2.4.0 && rvm use 2.4.0 --default && ruby -v"
     "Selenium":
         "pip install pyvirtualdisplay selenium",
+    "Gimp":
+        "sudo add-apt-repository ppa:otto-kesselgulasch/gimp -y" +
+        " && sudo apt-get update && sudo apt-get install gimp -y",
 
 }
